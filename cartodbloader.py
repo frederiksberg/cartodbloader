77262a87fae119ac9dfdfd04c60af11c588df552
Niels Kjøller Hansen <niels.k.h@gmail.com>
2016-09-14 10:25:40 +0200

    Migrate from OptParse to ArgParse

--- cartodbloader.py
+++ cartodbloader.py
@@ -28,7 +28,7 @@ It is propbably not very memory efficient, and should not be used with large
 datasets. It is also currently dreadfully slow
 """
 
-import json, urllib, sys
+import json, urllib, sys, argparse
 from shapely import wkb, geometry
 from urllib2 import Request, urlopen, URLError
 from optparse import OptionParser
@@ -72,7 +72,7 @@ def feature_to_values(feature, properties, expected_geom_type, promote_to_multi)
     # return value list
     return(u'(' + u','.join(values) + u')')
 
-def run_query(q):
+def run_query(q, cartodb_api_key=None):
     """Function to run a query on the CartoDB SQL API"""
     # Do the encodes
     data = urllib.urlencode({'q':q.encode('utf8'),'api_key': cartodb_api_key,})
@@ -100,7 +100,7 @@ def main(cartodb_api_url, cartodb_layer, cartodb_api_key, data):
     # Check target geometry type. If not found, it is missing.
     # TODO: It would probably be a good idea to check if it has the expected attribute fields
     q = u"SELECT type FROM geometry_columns WHERE f_table_name = '{0}' AND f_geometry_column = 'the_geom'".format(cartodb_layer)
-    cdb_geom_result = run_query(q)
+    cdb_geom_result = run_query(q, cartodb_api_key)
     if not cdb_geom_result:
         print("Table does not seem to exist in CartoDB. Right now, this script only does updates. Now exiting.")
         sys.exit()
@@ -163,36 +163,36 @@ def main(cartodb_api_url, cartodb_layer, cartodb_api_key, data):
     print "Done"
 
 if __name__ == "__main__":
-    # OptParse setup
-    parser = OptionParser(usage="usage: %prog [options] target_table source_file")
-    parser.add_option("-k", "--apikey", dest="apikey", help="API key for CartoDB")
-    parser.add_option("-a", "--account", dest="account", default="frederiksberg", help="CartoDB account name")
-    parser.add_option("-u", "--url", dest="url", help="Custom URL endpoint")
-    parser.add_option("-c", "--chunk-size", type="int", dest="chunk_size", default=50, help="Chunk size of bulk inserts")
-    (options, args) = parser.parse_args()
+    # ArgParse setup
+    parser = argparse.ArgumentParser(description='Load a geojson file into Carto through the SQL API.')
 
-    # Check if required arguments are present
-    if not args or len(args) < 2:
-        parser.error("Not enough arguments")
-    if not options.apikey:
-        parser.error("An API key is required for writing data to CartoDB")
-    if not (bool(options.url) != bool(options.account)):
-        parser.error("You need to provide an account or a custom URL (and not both)")
+    # Positional arguments
+    parser.add_argument('cartodb_layer', metavar='target_table', help = "Target table in Carto")
+    parser.add_argument('input_file', metavar='source_file', help = "Target table in Carto")
 
-    # Setup basic parameters
-    input_file = args[1]
-    cartodb_layer = args[0]
-    cartodb_api_key = options.apikey
+    # Api-key. As this script mostly does writes, it is required
+    parser.add_argument('-k', '--apikey', dest='cartodb_api_key', help="API key for Carto", required=True)
 
+    # Connection to Carto. account and URL are mutually exclusive
+    url_group = parser.add_mutually_exclusive_group(required=True)
+    url_group.add_argument("-a", "--account", dest="account", default="frederiksberg", help="CartoDB account name")
+    url_group.add_argument("-u", "--url", dest="url", help="Custom URL endpoint")
+
+    # An advanced option. You should probably not fiddle with this.
+    parser.add_argument("-c", "--chunk-size", type=int, dest="chunk_size", default=50, help="Chunk size of bulk inserts")
+
+    # Parse the arguments
+    args = parser.parse_args()
     # If a custom url is provided, use it. Alternatively, construct one from account
-    if options.url:
+
+    if args.url:
         cartodb_api_url = url
     else:
-        cartodb_api_url = u'https://{0}.cartodb.com/api/v2/sql'.format(options.account)
+        cartodb_api_url = u'https://{0}.cartodb.com/api/v2/sql'.format(args.account)
 
     # Get data from input file
-    with open(input_file,'r') as f:
+    with open(args.input_file,'r') as f:
         data = json.load(f)
 
     # Actually do something
-    main(cartodb_api_url, cartodb_layer, cartodb_api_key, data)
+    main(cartodb_api_url, args.cartodb_layer, args.cartodb_api_key, data)
